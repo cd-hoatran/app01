836a7fa02187d32b4422b2595a7c99be102ecd3b
Hoa Tran <hoatran@chidoanh.com>
2025-04-12 13:27:27 +0800

    update code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 3000;
 
 // routes will go here
 app.get('/api/getUserInfo', function(req, res) {
